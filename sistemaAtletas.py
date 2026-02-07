7e61c2c36c67c3e9bb8b2d18c108e7988bc5fdcd
Renan Carneiro <perealca17@gmail.com>
2026-02-07 20:31:21 -0300

    Update sistemaAtletas.py

--- sistemaAtletas.py
+++ sistemaAtletas.py
@@ -1,16 +1,16 @@
 idade = int(input("Digite a idade do atleta: "))
 
 if idade >= 5 and idade <= 7:
-    categoria = "Infantil"
+categoria = "Infantil"
 elif idade >= 8 and idade <= 10:
-    categoria = "Iniciado"
+categoria = "Iniciado"
 elif idade >= 11 and idade <= 13:
-    categoria = "Juvenil"
+categoria = "Juvenil"
 elif idade >= 14 and idade <= 17:
-    categoria = "Junior"
+categoria = "Junior"
 elif idade >= 18:
-    categoria = "Sênior"
+categoria = "Sênior"
 else:
-    categoria = "Idade fora das categorias"
+categoria = "Idade fora das categorias"
 
 print("Categoria:", categoria)
